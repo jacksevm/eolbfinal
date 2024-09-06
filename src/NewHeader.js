41b451007cb0edf0b039bab9a8a292e938c6a486
jacksevm <87648664+jacksevm@users.noreply.github.com>
2024-09-06 18:17:50 +0530

    Update NewHeader.js

--- src/NewHeader.js
+++ src/NewHeader.js
@@ -22,7 +22,7 @@ const NewHeader = () => {
         {isMenuOpen && (
           <ul className="menu-items">
             <li><Link to="/" onClick={toggleMenu}>Home</Link></li>
-            <li><Link to="/workprogress" onClick={toggleMenu}>Work Progress</Link></li>
+          {/*<li><Link to="/workprogress" onClick={toggleMenu}>Work Progress</Link></li>*/}
             <li><Link to="/eolb" onClick={toggleMenu}>EOLB</Link></li>
             <li><Link to="/eolbchecklist" onClick={toggleMenu}>EOLB Pre-com Check lists</Link></li>
           {/*<li><Link to="https://public.tableau.com/app/profile/jackson.pereira/viz/FailureAnalysis-MASDivision/Dashboard2" onClick={toggleMenu}>Failure Analysis</Link></li>*/}
