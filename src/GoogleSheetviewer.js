86f981eb0075cfa2139bdd98c9795f5bd3d01521
jacksevm <87648664+jacksevm@users.noreply.github.com>
2025-01-25 01:28:23 +0530

    Create GoogleSheetviewer.js

---
+++ src/GoogleSheetviewer.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+
+
+function GoogleSheetEmbed() {
+
+  const sheetUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTaKOyz-I2Z2k3ge43s3jDDkD_bCa4776Af34YF7cqMe53f_aE34rvVBJwT3AmULQ/pubhtml'; // Replace with your sheet's published URL
+
+
+
+  return (
+
+    <iframe
+
+      title="Embedded Google Sheet"
+
+      src={sheetUrl}
+
+      width="600"
+
+      height="400"
+
+      style={{ border: 'none' }}
+
+    ></iframe>
+
+  );
+
+}
+
+
+
+export default GoogleSheetEmbed;
