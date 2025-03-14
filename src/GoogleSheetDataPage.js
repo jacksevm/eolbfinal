b003d578dde19195f1cb6e23f7d2cab55b76e0d4
jacksevm <87648664+jacksevm@users.noreply.github.com>
2025-03-14 09:51:46 +0530

    Update GoogleSheetDataPage.js

--- src/GoogleSheetDataPage.js
+++ src/GoogleSheetDataPage.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 
 const GoogleSheetDataPage = () => {
   const [data, setData] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
   const sheetURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTbNntbhjHIZSDdFgXbBD5JuauDfu7kHK7CsKbQ3Il-hiTKtyInc8h0HpraGeZPp_tll8y0RGWufLcN/pub?output=csv';
 
   useEffect(() => {
@@ -14,24 +15,40 @@ const GoogleSheetDataPage = () => {
       .catch(error => console.error('Error fetching Google Sheet:', error));
   }, []);
 
+  const filteredData = data.slice(1).filter(row =>
+    row[0]?.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   return (
     <div className="page-container">
-      <h2 className="page-title">FAT DATA </h2>
-    <h2 className="page-title">(DDS BETWEEN CGL to VM)</h2>
+      <h2 className="page-title">
+        FAT DATA <br /> (DDS BETWEEN CGL to VM)
+      </h2>
+      <input
+        type="text"
+        placeholder="Search by station name"
+        value={searchQuery}
+        onChange={e => setSearchQuery(e.target.value)}
+        className="search-bar"
+      />
       <div className="table-container">
         <table className="data-table">
           <thead>
             <tr className="table-header">
               {data[0] && data[0].map((header, index) => (
-                <th key={index}>{header}</th>
+                <th key={index}>
+                  {header}
+                </th>
               ))}
             </tr>
           </thead>
           <tbody>
-            {data.slice(1).map((row, rowIndex) => (
+            {filteredData.map((row, rowIndex) => (
               <tr key={rowIndex} className="table-row">
                 {row.map((cell, cellIndex) => (
-                  <td key={cellIndex} className="table-cell">{cell}</td>
+                  <td key={cellIndex} className="table-cell">
+                    {cell}
+                  </td>
                 ))}
               </tr>
             ))}
