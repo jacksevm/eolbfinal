3dc10dbdc8b354ead26c7eeb3d7a046f8f910eff
jacksevm <87648664+jacksevm@users.noreply.github.com>
2025-04-17 10:34:59 +0530

    Create WorkProgressmyp.js

---
+++ src/WorkProgressmyp.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+
+
+function WorkProgressmyp() {
+
+  const sheetUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTaKOyz-I2Z2k3ge43s3jDDkD_bCa4776Af34YF7cqMe53f_aE34rvVBJwT3AmULQ/pubhtml'; // Replace with your sheet's published URL
+
+  return (
+
+
+      <div className='App'>
+    <div className='container'>
+       
+        <div className="button-container">
+          <button className="google-sheets-button" onClick={() => window.open("https://docs.google.com/spreadsheets/d/16qa7ixZ4wBWGv-kaGfx8M7RlAp91Wvve/edit?gid=1653570427#gid=1653570427", '_blank')}>
+            Open Google Sheet
+          </button>
+        </div>
+       <iframe
+
+      title="MYP - WORK PROGRESS"
+
+      src={sheetUrl}
+
+      width="100%"
+
+      height="600"
+
+      style={{ border: 'none' }}
+
+    ></iframe>
+      </div>
+      </div>
+
+  );
+
+}
+
+
+
+export default WorkProgressmyp;
