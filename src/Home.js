d20fcfd1e30f101ed07ddf5cc7b1f6b534a78ffa
jacksevm <87648664+jacksevm@users.noreply.github.com>
2025-04-17 11:36:15 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -42,7 +42,7 @@ function Home() {
                 <div className="card">
           <Link to="/workprogressmyp">
             <img
-              src={process.env.PUBLIC_URL + '/img/Mundiyampakkam_Railway_Station.jpg'}
+              src={process.env.PUBLIC_URL + '/img/station.jpg'}
               alt="MYP Work Progress"
             />
             <div className="card-info">
