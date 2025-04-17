26261748f8a8dae75ef93dfc980f26f6e83f4c04
jacksevm <87648664+jacksevm@users.noreply.github.com>
2025-04-17 10:39:38 +0530

    Update WorkProgressmyp.js

--- src/WorkProgressmyp.js
+++ src/WorkProgressmyp.js
@@ -4,7 +4,7 @@ import React from 'react';
 
 function WorkProgressmyp() {
 
-  const sheetUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTaKOyz-I2Z2k3ge43s3jDDkD_bCa4776Af34YF7cqMe53f_aE34rvVBJwT3AmULQ/pubhtml'; // Replace with your sheet's published URL
+  const sheetUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRVQkF1J0pVDaa-DihKg4ySVVN9P_H3D_yjrSPibywI8MG1BfiWIFfRTpmrDEt4zg/pubhtml'; // Replace with your sheet's published URL
 
   return (
 
@@ -13,7 +13,7 @@ function WorkProgressmyp() {
     <div className='container'>
        
         <div className="button-container">
-          <button className="google-sheets-button" onClick={() => window.open("https://docs.google.com/spreadsheets/d/16qa7ixZ4wBWGv-kaGfx8M7RlAp91Wvve/edit?gid=1653570427#gid=1653570427", '_blank')}>
+          <button className="google-sheets-button" onClick={() => window.open("https://docs.google.com/spreadsheets/d/1T7hyIG7s2v7lGb5vo9-4VRHnaOdtm8-b/edit?gid=1653570427#gid=1653570427", '_blank')}>
             Open Google Sheet
           </button>
         </div>
