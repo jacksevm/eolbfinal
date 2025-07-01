3f2e4cc39f27ce9a452345976afb4180e704b7a1
jacksevm <87648664+jacksevm@users.noreply.github.com>
2025-07-01 13:20:29 +0530

    Update NewHeader.js

--- src/NewHeader.js
+++ src/NewHeader.js
@@ -22,9 +22,9 @@ const NewHeader = () => {
         {isMenuOpen && (
           <ul className="menu-items">
             <li><Link to="/" onClick={toggleMenu}>Home</Link></li>
+          <li><Link to="/eolb" onClick={toggleMenu}>EOLB Status</Link></li>
           <li><Link to="/workprogress" onClick={toggleMenu}>DDS Work Progress</Link></li>
-            <li><Link to="/workprogressmyp" onClick={toggleMenu}>MYP Work Progress</Link></li>
-             {/*<li><Link to="/eolb" onClick={toggleMenu}>EOLB Status</Link></li>
+            {/* <li><Link to="/workprogressmyp" onClick={toggleMenu}>MYP Work Progress</Link></li>            
          <li><Link to="/fat" onClick={toggleMenu}>FAT TESTING DATA</Link></li>
             {/* <li><Link to="/eolbchecklist" onClick={toggleMenu}>EOLB Pre-com Check lists</Link></li>*/}
           
